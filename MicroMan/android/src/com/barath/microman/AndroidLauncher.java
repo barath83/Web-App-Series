85320aaf5739f45d989c99297fb66f85bd431f74
Barath Gopinath <barathgopi1699@gmail.com>
2020-05-09 10:02:25 +0530

    second app

---
+++ MicroMan/android/src/com/barath/microman/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.barath.microman;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.barath.microman.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
