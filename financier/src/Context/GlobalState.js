2f6db7507a9fe82c7d0e890ede6b7785166b6472
Barath Gopinath <barathgopi1699@gmail.com>
2020-05-26 00:42:53 +0530

    first commit

---
+++ financier/src/Context/GlobalState.js
@@ -0,0 +1,34 @@
+import React,{createContext,useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+//Initial state
+const initialState = {
+    transactions :  []
+}
+
+//Create context
+export const GlobalContext = createContext(initialState);
+
+//Provider component
+ export const GlobalProvider = ({children}) => {
+    const[state,dispatch] = useReducer(AppReducer,initialState);
+
+    //Actions
+    function addTransaction(transaction){
+        dispatch({
+            type : 'ADD_TRANSACTION',
+            payload : transaction
+        });
+    }
+
+    function deleteTransaction(id){
+        dispatch({
+            type : 'DELETE_TRANSACTION',
+            payload : id
+        });
+    }
+
+    return(<GlobalContext.Provider value={{transactions:state.transactions,deleteTransaction,addTransaction}}>
+        {children}
+    </GlobalContext.Provider>);
+ }
---
+++ financier/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2>
+         Financier   
+        </h2>
+    )
+}
+
+export default Header;
